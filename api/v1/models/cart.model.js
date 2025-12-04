3e0da0134635d7bef826c6d6b686630987e66139
PhamHuynh1612 <phamhuynh1612@gmail.com>
2025-12-04 11:16:07 +0700

    ok

--- api/v1/models/cart.model.js
+++ api/v1/models/cart.model.js
@@ -1,34 +1,34 @@
 const mongoose = require("mongoose");
 
 const cartItemSchema = new mongoose.Schema({
-    product_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Product",
-        required: true
-    },
-    quantity: {
-        type: Number,
-        default: 1,
-        min: 1
-    }
+  product_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    default: 1,
+    min: 1,
+  },
 });
 
 const cartSchema = new mongoose.Schema(
-    {
-        userToken: {
-            type: String, // Lưu token của user thay vì user_id
-            required: true,
-            unique: true
-        },
-        items: [cartItemSchema],
-        deleted: {
-            type: Boolean,
-            default: false
-        }
+  {
+    userToken: {
+      type: String, // Lưu token của user thay vì user_id
+      required: true,
+      unique: true,
     },
-    {
-        timestamps: true
-    }
+    items: [cartItemSchema],
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 const Cart = mongoose.model("Cart", cartSchema, "carts");
--- api/v1/models/product-category.model.js
+++ api/v1/models/product-category.model.js
@@ -1,34 +1,40 @@
 const mongoose = require("mongoose");
-var slug = require("mongoose-slug-updater")
+var slug = require("mongoose-slug-updater");
 
-mongoose.plugin(slug)
+mongoose.plugin(slug);
 
 const productCategorySchema = new mongoose.Schema(
   {
     title: String,
     parent_id: {
       type: String,
-      default: ""
+      default: "",
     },
     description: String,
     status: String,
     slug: {
       type: String,
       slug: "title",
-      unique: true
+      unique: true,
     },
     deleted: {
       type: Boolean,
-      default: false
+      default: false,
     },
-    deletedAt: Date
-  }, {
-  timestamps: {
-    createdAt: "created_at",
-    updatedAt: "updated_at"
+    deletedAt: Date,
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
   }
-});
+);
 
-const ProductCategory = mongoose.model('ProductCategory', productCategorySchema, "products-category");
+const ProductCategory = mongoose.model(
+  "ProductCategory",
+  productCategorySchema,
+  "products-category"
+);
 
 module.exports = ProductCategory;
